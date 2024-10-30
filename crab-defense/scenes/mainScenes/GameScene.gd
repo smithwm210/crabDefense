7138ceed6c31e73903673e5ac064912b391482b3
vcmistry03 <vcmistry@wpi.edu>
2024-10-30 18:31:08 -0400

    tutorial_build_1
    did tutorial 1-6, need to go back to scene manager and implement from there

---
+++ crab-defense/scenes/mainScenes/GameScene.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+var map_node
+
+var current_wave = 0
+var enemies_in_wave = 0
+
+func _ready():
+	map_node = get_node("map_1")
+	start_next_wave()
+
+##
+## Wave Functions
+##
+
+func start_next_wave():
+	var wave_data = retrieve_wave_data()
+	await get_tree().create_timer(0.2)
+	spawn_enemies(wave_data)
+	
+func retrieve_wave_data():
+	var wave_data = [["BlueTank",0.7],["BlueTank",0.7]]
+	current_wave += 1
+	enemies_in_wave = wave_data.size()
+
+func spawn_enemies(wave_data):
+	for i in wave_data:
+		var new_enemy = load("res://scenes/enemies/" + i[0] + ".tscn").instance()
+		map_node.get_node("path").add_child(new_enemy, true)
+		await get_tree().create_timer(i[1])
