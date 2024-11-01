8ab1cca48dfa7360302d206afc060d9f8ac075d8
vcmistry03 <vcmistry@wpi.edu>
2024-11-01 09:53:27 -0400

    im tired
    tried to add shoot functionality

--- crab-defense/scenes/mainScenes/GameScene.gd
+++ crab-defense/scenes/mainScenes/GameScene.gd
@@ -15,17 +15,17 @@ func _ready():
 
 func start_next_wave():
 	var wave_data = retrieve_wave_data()
-	await get_tree().create_timer(0.2)
+	get_tree().create_timer(0.2)
 	spawn_enemies(wave_data)
 	
 func retrieve_wave_data():
-	var wave_data = [["BlueTank",1.2],["BlueTank",0.5]]
+	var wave_data = [["BlueTank",1],["BlueTank",0.2]]
 	current_wave += 1
 	enemies_in_wave = wave_data.size()
 	return wave_data
 
 func spawn_enemies(wave_data):
 	for i in wave_data:
-		var new_enemy = load("res://scenes/enemies/node_2d.tscn").instantiate()
+		var new_enemy = load("res://scenes/enemies/"+i[0]+".tscn").instantiate()
 		map_node.get_node("path").add_child(new_enemy, true)
-		await get_tree().create_timer(i[1])
+		get_tree().create_timer(i[1])
--- crab-defense/scenes/turrets/turrets.gd
+++ crab-defense/scenes/turrets/turrets.gd
@@ -1,9 +1,40 @@
 extends Node2D
 
 
+var type
+var enemy_array = [1]
+#var built = false
+var enemy
+var readied = true
+
+
 func _physics_process(delta):
-	turn()
+	if enemy_array.size() != 0:
+		enemy = enemy_array[0]
+		#select_enemy()
+		turn()
+		if readied:
+			fire()
+	else:
+		enemy = null
+
 
 func turn():
+	#get_node("Turret").look_at(enemy.position)
 	var enemy_position = get_global_mouse_position()
 	get_node("Turret").look_at(enemy_position)
+
+#func select_enemy():
+	#var enemy_progress_array = []
+	#for i in enemy_array:
+		#enemy_progress_array.append(i.offset)
+	#var max_offset = enemy_progress_array.max()
+	#var enemy_index = enemy_progress_array.find(max_offset)
+	#enemy = enemy_array[enemy_index]
+
+func fire():
+	readied = false
+	#enemy.on_hit(GameData.tower_data["GunT1"]["damage"])
+	get_tree().create_timer(1)
+	readied = true
+	
