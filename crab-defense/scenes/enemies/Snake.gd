9aae99218308aa965e43d0656f623092a8b75fc0
William Smith <116986531+smithwm210@users.noreply.github.com>
2024-11-04 17:03:34 -0500

    added snake and earth turret

---
+++ crab-defense/scenes/enemies/Snake.gd
@@ -0,0 +1,19 @@
+extends PathFollow2D
+
+
+var speed = 150
+var hp = 50
+
+func _physics_process(delta):
+	move(delta)
+
+func move(delta):
+	set_progress(get_progress() + speed * delta)
+	
+func on_hit(damage):
+	hp -= damage
+	if hp <= 0:
+		on_destroy()
+
+func on_destroy():
+	self.queue_free()
